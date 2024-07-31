d7c36268a69059687cbe6a07ad108ce0f7e14bcf
ketan-zore22 <kkzore22@gmail.com>
2024-08-01 00:10:08 +0530

    B.java created by dev1

---
+++ B.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class B{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st number : ");
+		int a = sc.nextInt();
+		System.out.println("Enter 2nd number : ");
+		int b = sc.nextInt();
+		int c = a + b;
+		System.out.println("Addition is : "+ c);
+		int d = a - b;
+		System.out.println("Addition is : "+ d);
+		int e = a * b;
+		System.out.println("Addition is : "+ e);
+		int f = a / b;
+		System.out.println("Addition is : "+ f);
+
+
+
+		
+		
+		System.out.println(c);
+
+
+                
+	}
+}
